87a7722d145c8ae16be3cdf14dda9191d91d446a
Pavel-Kalashnikoff <Pavel.alex.ivanov.13@mail.ru>
2024-01-16 17:46:01 +0300

    tasks-3

--- tasks-js.js
+++ tasks-js.js
@@ -4,5 +4,13 @@ a = 20;
 alert(a);
 let year = 2007;
 alert(year);
-let nameCreatorJS = BrendanEich;
+
+let nameCreatorJS = 'Brendan Eich';
 alert(nameCreatorJS);
+
+let b = 10, c = 2;
+alert(b+c);
+alert(b-c);
+alert(b*c);
+alert(b/c);
+
