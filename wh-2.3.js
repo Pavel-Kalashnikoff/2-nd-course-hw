55712f8fd508720fcd0e5d34ea8256bc43bcac3d
Pavel-Kalashnikoff <Pavel.alex.ivanov.13@mail.ru>
2024-01-23 21:46:59 +0300

    Corrected version

--- wh-2.3.js
+++ wh-2.3.js
@@ -15,8 +15,8 @@ if (c>0 && c<10) {
 }
 
 
-let d = 34, e = 593;
-if (d||e > 100) {
+let d = 34, e = 222;
+if (d > 100 || e > 100) {
 	alert ('Верно');
 } else {
 	alert ('Неверно');
@@ -32,19 +32,27 @@ alert (parseInt(a) + parseInt(b));
 let monthNumber = prompt('Напишите номер любого месяца в году');
 
 switch (monthNumber) {
-	case '12' && '1' && '2':
-		console.log('Этот месяц отностится к зимнему сезону');
-		break;
-		case '3' && '4' && '5':
-			console.log('Этот месяц отностится к весеннему сезону');
-			break;
-			case '6' && '7' && '8':
-			console.log('Этот месяц отностится к летнему сезону');
-			break;
-			case '9' && '10' && '11':
-			console.log('Этот месяц отностится к осеннему сезону');
-			break;
+	case '12':
+	case '1':
+	case '2':
+	console.log('Этот месяц отностится к зимнему сезону');
+	break;
+	case '3':
+	case '4':
+	case '5':	
+	console.log('Этот месяц отностится к весеннему сезону');
+	break;
+	case '6':
+	case '7':
+	case '8':
+	console.log('Этот месяц отностится к летнему сезону');
+	break;
+	case '9':
+	case '10':
+	case '11':
+	console.log('Этот месяц отностится к осеннему сезону');
+	break;
 	default:
-		console.log ('Данные введены неверно')
-		break;
+	console.log ('Данные введены неверно')
+	break;
 }
