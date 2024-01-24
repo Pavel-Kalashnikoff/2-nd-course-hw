0d3f8368a36ce2963840149d7dc22a10e55231cc
Pavel-Kalashnikoff <Pavel.alex.ivanov.13@mail.ru>
2024-01-24 17:34:59 +0300

    4 task

--- wh-2.4.js
+++ wh-2.4.js
@@ -11,3 +11,13 @@ for (let i = 1; i <= 5; i++) {
 for (let i_2 = 7; i_2 <= 22; i_2++) {
 	console.log (i_2);
 }
+
+const obj = {
+	Коля: "200",
+	Вася: "300",
+	Петя: "400",
+}
+
+for (let salary in obj) {
+	alert(`${salary} - зарплата ${obj[salary]} долларов.`)
+}
