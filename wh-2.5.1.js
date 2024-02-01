372a21751dfd1553dc2c46d7515db66ef80b9d0b
Pavel-Kalashnikoff <156201853+Pavel-Kalashnikoff@users.noreply.github.com>
2024-02-01 08:07:29 +0000

    1 task

---
+++ wh-2.5.1.js
@@ -0,0 +1,20 @@
+function minNumber(a, b) {
+  return Math.min(a, b);
+}
+console.log(minNumber(8, 4));
+console.log(minNumber(9, 2));
+
+function even (nubmer) {
+	if (nubmer % 2 === 0) {
+		return 'Четное число';
+	} else {
+		return 'Нечетное число';
+	} 
+}
+console.log (even (8));
+console.log (even (7));
+
+function square(result) {
+	let a = prompt ('Введите число');
+	console.log ()
+}
