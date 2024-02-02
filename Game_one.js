e88f07901b0224946c81ca87282164b45b55c49e
Pavel-Kalashnikoff <156201853+Pavel-Kalashnikoff@users.noreply.github.com>
2024-02-02 10:06:18 +0000

    Full completion of dz, added animations to buttons

--- Game_one.js
+++ Game_one.js
@@ -1,18 +1,17 @@
 function GameOne() {
-	let monthNumber = prompt("Напишите любой номер месяца");
-	if (monthNumber === 12 || 1 || 2) {
+	let monthNumber = Number(prompt("Напишите любой номер месяца"));
+	if (monthNumber === 12 || monthNumber === 1 || monthNumber === 2) {
 		alert ('Значит, вам нравится зима :)');
-	} else if (monthNumber === 3 || 4 || 5) {
+	} else if (monthNumber === 3 || monthNumber === 4 || monthNumber === 5) {
 		alert ('Значит, вам нравится весна :)');
-	} else if (monthNumber === 6 || 7 || 8) {
+	} else if (monthNumber === 6 || monthNumber === 7 || monthNumber === 8) {
 		alert ('Значит, вам нравится лето :)');
-	} else if (monthNumber === 9 || 10 || 11) {
+	} else if (monthNumber === 9 || monthNumber === 10 || monthNumber === 11) {
 		alert ('Значит, вам нравится осень :)');
 	} else if (monthNumber >= 13) {
 		alert ('Вы ввели неверное значение');
 	}
 }
-
 // function GameOne() {
 // 	let monthNumber = prompt('Напишите любой номер месяца');
 // switch (monthNumber) {
