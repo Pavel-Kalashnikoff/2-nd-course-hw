e77cd0c0771b9e37637531f4e914772042de5a15
Pavel-Kalashnikoff <156201853+Pavel-Kalashnikoff@users.noreply.github.com>
2024-02-02 08:53:56 +0000

    Recovered the file from javascript

--- Game_one.js
+++ Game_one.js
@@ -1,29 +1,43 @@
 function GameOne() {
-	let monthNumber = prompt('Напишите любой номер месяца');
-	
-switch (monthNumber) {
-	case '12':
-	case '1':
-	case '2':
-	alert('Значит, вам нравится зима :)');
-	break;
-	case '3':
-	case '4':
-	case '5':	
-	alert('Значит, вам нравится весна :)');
-	break;
-	case '6':
-	case '7':
-	case '8':
-		alert('Значит, вам нравится лето :)');
-	break;
-	case '9':
-	case '10':
-	case '11':
-		alert('Значит, вам нравится осень :)');
-	break;
-	default:
-		alert('Вы ввели неверное значение');
-	break;
+	let monthNumber = prompt("Напишите любой номер месяца");
+	if (monthNumber === 12 || 1 || 2) {
+		alert ('Значит, вам нравится зима :)');
+	} else if (monthNumber === 3 || 4 || 5) {
+		alert ('Значит, вам нравится весна :)');
+	} else if (monthNumber === 6 || 7 || 8) {
+		alert ('Значит, вам нравится лето :)');
+	} else if (monthNumber === 9 || 10 || 11) {
+		alert ('Значит, вам нравится осень :)');
+	} else if (monthNumber >= 13) {
+		alert ('Вы ввели неверное значение');
+	}
 }
-}
+
+// function GameOne() {
+// 	let monthNumber = prompt('Напишите любой номер месяца');
+// switch (monthNumber) {
+// 	case '12':
+// 	case '1':
+// 	case '2':
+// 	alert('Значит, вам нравится зима :)');
+// 	break;
+// 	case '3':
+// 	case '4':
+// 	case '5':	
+// 	alert('Значит, вам нравится весна :)');
+// 	break;
+// 	case '6':
+// 	case '7':
+// 	case '8':
+// 		alert('Значит, вам нравится лето :)');
+// 	break;
+// 	case '9':
+// 	case '10':
+// 	case '11':
+// 		alert('Значит, вам нравится осень :)');
+// 	break;
+// 	default:
+// 		alert('Вы ввели неверное значение');
+// 	break;
+// }
+// }
--- wh-2.5.1.js
+++ wh-2.5.1.js
@@ -1,5 +1,5 @@
 function minNumber(a, b) {
-  return Math.min(a, b);
+	return Math.min(a, b);
 }
 console.log(minNumber(8, 4));
 console.log(minNumber(9, 2));
@@ -9,21 +9,81 @@ function even (nubmer) {
 		return 'Четное число';
 	} else {
 		return 'Нечетное число';
-	} 
+	}
 }
 console.log (even (8));
 console.log (even (7));
 
 
 function squarNumber(number) {
-	let squar = number * number;
+	let squar = Math.pow(number, 2);
 	console.log ("Квадрат числа", number, "это", squar);
 }
+squarNumber(6);
 
-// function squar(number) {
-// 	let result = number ** 2;
-// 	return result
-// }
+function squarN(Parameter) {
+return Math.pow (Parameter, 2);
+}
+console.log (squarN(4));
 
-// console.log (squar(5));
-// console.log (squar(9));
+
+Questionnaire()
+function Questionnaire() {
+	let age = parseInt(prompt("Здравствуйте, сколько вам лет?"));
+		if (age < 0) {
+		alert("Вы ввели неверное значение");
+	} else if (age >= 0 && age <= 12) {
+		alert("Привет, друг!");
+	}
+	else if (age >= 13) {
+		alert("Добро пожаловать!");
+	}
+}
+
+
+function Examination(a, b) {
+	if (isNaN(a) || isNaN(b)) {
+		return 'Одно или оба значения не являются числом';
+	} else {
+		return a * b;
+	}
+}
+console.log (Examination (5, 10));
+console.log (Examination ('a', 10));
+
+function Number() {
+	let n = prompt("Введите любое число");
+	if (isNaN(n)) {
+		return 'Переданный параметр не является числом';
+	} else {
+	let result = Math.pow(n, 3);
+	return `${n} в кубе равняется ${result}`;
+	}
+	}
+
+console.log (Number())
+
+let circle1 = {
+	r: 4,
+	getArea: function() {
+		return Math.PI * this.r ** 2;
+	},
+	getPerimeter: function() {
+		return 2 * Math.PI * this.r;
+}
+};
+
+let circle2 = {
+	r: 4,
+	getArea: function() {
+		return Math.PI * this.r ** 2;
+	},
+	getPerimeter: function() {
+		return 2 * Math.PI * this.r;
+	}
+};
+console.log (circle1.getArea());
+console.log (circle1.getPerimeter());
+
+console.log (circle2.getArea());
+console.log (circle2.getPerimeter());
