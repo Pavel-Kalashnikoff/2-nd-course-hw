98557f7f415a529f52fcfe1fcc1762112fe4fddb
Pavel-Kalashnikoff <Pavel.alex.ivanov.13@mail.ru>
2024-02-13 14:06:29 +0300

    exercise 3

--- Task/wh-2.7.js
+++ Task/wh-2.7.js
@@ -40,3 +40,22 @@ const res = search();
 console.log(res);
 // не очень понял, но очень интересно. Необходимо потренироваться с методами
 
+
+//Задание №3 - Округлите число
+// let number = 32.58884
+// let a = Math.floor(number);
+// console.log(a);
+// let b = Math.ceil(number);
+// console.log(b);
+// let c = Math.round(number);
+// console.log(c);
+// Решил сделать код более компактно 
+let number = 32.58884
+let a = Math.floor(number);
+let b = Math.ceil(number);
+let c = Math.round(number);
+
+console.log(a);
+console.log(b);
+console.log(c);
+
