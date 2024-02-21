2ef8b1972f5425013529fd84e302ad773706e650
Pavel-Kalashnikoff <Pavel.alex.ivanov.13@mail.ru>
2024-02-21 15:46:44 +0300

    2 exercise

--- Task/wh-2.8.js
+++ Task/wh-2.8.js
@@ -1,4 +1,4 @@
-//Задание №1 Отсортировать массив по возрастанию возраста и вывести в консоль
+//Задание № 1 Отсортировать массив по возрастанию возраста и вывести в консоль
 
 const people = [
 	{ name: 'Глеб', age: 29 },
@@ -9,10 +9,38 @@ const people = [
 
 
 console.log(people.sort((a, b) => a.age - b.age)); // использовал два параметра для sort и обратился через них к age 
-// код выше должен вывеcти =>
-// [
-//  { name: 'Олег', age: 7 },
-//  { name: 'Анна', age: 17 },
-//  { name: 'Глеб', age: 29 },
-//  { name: 'Оксана', age: 47 }
-// ]
+
+
+// Задание № 2 написать функцию которая работает как метод filter
+
+function isPositive(num) {
+	return num > 0
+	}
+
+	function isMale(el) {
+	return el.gender === 'male'
+	}
+
+	function filter(arr, ruleFunction) {
+	const filterArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (ruleFunction(arr[i])) {
+			filterArr.push(arr[i]);
+		}
+	}
+	return filterArr;
+	}
+	
+	console.log(filter([3, -4, 1, 9], isPositive)); // Должен выводить [3, 1, 9]
+	
+	const people_1 = [
+		 {name: 'Глеб', gender: 'male'},
+		 {name: 'Анна', gender: 'female'},
+		 {name: 'Олег', gender: 'male'},
+		 {name: 'Оксана', gender: 'female'}
+	];
+	
+	console.log(filter(people_1, isMale)); // Должен выводить [{name: 'Глеб', gender: 'male'},  {name: 'Олег', gender: 'male'}]
+
+
+	
